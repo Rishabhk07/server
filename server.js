ca42b75dc57863cfffc0d890aa9f36cd37d52e52
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-07-30 16:44:26 +0530

    herolu port changed
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
  * Created by rishabhkhanna on 30/07/18.
  */
 const app = require('express')();
+const PORT = process.env.PORT || 9090
 
 app.get("/", (req,res)=>{
     res.send("<h1>hello from backend</h1>");
 })
 
-app.listen(9090, ()=>{
+app.listen(PORT, ()=>{
     console.log("server has started");
 })
 
